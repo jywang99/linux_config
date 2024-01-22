ca6a821c1467c22f973216a3c99a8e43cb111d43
Junyi Wang <junyi.wang.007@gmail.com>
2024-01-21 21:45:47 -0700

    tmux change

--- tmux/dev_tmux.sh
+++ tmux/dev_tmux.sh
@@ -10,9 +10,14 @@ fi
 
 tmux new-session -d -s $SESSION_NAME
 
+tmux new-window -t $SESSION_NAME:0 -n 'config'
 tmux new-window -t $SESSION_NAME:1 -n 'config'
 tmux new-window -t $SESSION_NAME:2 -n 'proj'
 tmux new-window -t $SESSION_NAME:3 -n 'deploy'
+
+# For some reason, the first window doesn't get renamed without this'
+tmux select-window -t 1
+tmux kill-window
 
 tmux attach-session -t $SESSION_NAME
 
